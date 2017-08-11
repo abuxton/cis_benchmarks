9f857c6c0b345061950b6273cf011710ace49f88
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 10:09:13 +0100

    /var/log

---
+++ manifests/redhat7/rule/v_2_1_1/rule_1_1_11.pp
@@ -0,0 +1,15 @@
+# 1.1.11 Ensure separate partition exists for /var/log (Scored)
+# 1.1.11 Ensure separate partition exists for /var/log (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_11{
+file_line { '(1.1.11) /var/log is separate mount':
+  path  => '/etc/fstab',
+  line  => '#<volume>  /var/log  <fstype> nodev 0 0',
+  #match => '\*/var/log\*nodev',
+  }
+  file_line { '(1.1.12) /var/log/audit is separate mount':
+    path  => '/etc/fstab',
+    line  => '#<volume>  /var/log/audit  <fstype> nodev 0 0',
+    #match => '\*/var/log/audit',
+  }
+
+}#eof
