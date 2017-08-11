48f126bd13360b1cbafd9416c10a4a6070f2cb23
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 16:31:23 +0100

    disable avahi

---
+++ manifests/redhat7/rule/v_2_1_1/rule_2_2_3.pp
@@ -0,0 +1,9 @@
+# 2.2.3 Ensure Avahi Server is not enabled (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_2_2_3 (
+  exec{ '2.2.3 - Disable Avahi (Scored)':
+    command => 'systemctl disable avahi-damon',
+    path    => '/sbin:/bin',
+    onlyif  => 'test [$(systemctl is-enabled avahi-daemon) == "enabled"]',
+  }
+
+} #EOF
