ac406a134312633401dce3008bb882d360f8b4a9
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 16:57:56 +0100

    disable services

---
+++ manifests/redhat7/rule/v_2_1_1/rule_2_2_14.pp
@@ -0,0 +1,8 @@
+# Ensure SNMP Server is not enabled (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_2_2_13 {
+  exec{ '2.2.14 - Disable snmpd (Scored)':
+    command => 'systemctl disable snpd',
+    path    => '/sbin:/bin',
+    onlyif  => 'test [$(systemctl is-enabled snmpd) == "enabled"]',
+  }
+} #EOF
