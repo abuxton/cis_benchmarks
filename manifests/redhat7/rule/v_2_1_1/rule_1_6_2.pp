063d06758a51c256718b3364bd82cff5f2f3aec9
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 14:03:31 +0100

    update all banners to remove identifiers

--- manifests/redhat7/rule/v_2_1_1/rule_1_6_2.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_6_2.pp
@@ -1,5 +1,5 @@
 1.6.2 Ensure SELinux is installed (Scored)
-class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_5_4 {
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_6_2 {
   package{ '1.6.2 Ensure libselinux is installed (Scored)':
     name    => 'libselinux',
     ensure  => installed,
---
+++ manifests/redhat7/rule/v_2_1_1/rule_1_7_1.pp
@@ -0,0 +1,4 @@
+# 1.6.2 Ensure SELinux is installed (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_7_1 {
+
+} #EOF
