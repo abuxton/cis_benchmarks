52a17a9b09ac7c39b4289cfedd96b3f728b19d4d
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 13:37:14 +0100

    prelink removed

--- manifests/redhat7/rule/v_2_1_1/rule_1_5_4.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_5_4.pp
@@ -1,20 +1,7 @@
-# 1.5.3 Ensure address space layout randomization (ASLR) is enabled
-class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_5_3 {
-  $file = '/etc/sysctl.conf'
-  if !defined(File[$files]){
-    file { "(1.5.1) - ${file} exists":
-      ensure => file,
-      path   => $file,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
-    }
+# 1.5.4 Ensure prelink is disabled (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_5_4 {
+  package{ '1.5.4 Ensure prelink is disabled (Scored)':
+    name    => 'prelink',
+    ensure  => purged,
   }
-  file_line { "(1.5.3) - ${file_sysctl}: enable ASLR":
-    ensure => present,
-    path   => $file,
-    line   => 'kernel.randomize_va_space = 2',
-    match  => '^kernel.randomize_va_space',
-  }
-
-  } #EOF
+} #EOF
