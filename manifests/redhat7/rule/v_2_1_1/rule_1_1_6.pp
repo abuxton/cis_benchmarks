9c8f0f23a51abdec2d324e92d6b269652aefaf28
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 09:39:46 +0100

    add rules for /var

---
+++ manifests/redhat7/rule/v_2_1_1/rule_1_1_6.pp
@@ -0,0 +1,23 @@
+# 1.1.6 Ensure separate partition exists for /var (Scored)
+# 1.1.7 Ensure separate partition exists for /var/tmp (Scored)
+# 1.1.8 Ensure nodev option set on /var/tmp partition (Scored
+# 1.1.9 Ensure nosuid option set on /var/tmp partition (Scored)
+# 1.1.10 Ensure noexec option set on /var/tmp partition (Scored)
+
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_6{
+  file_line { '(1.1.6) /var is mounted':
+    ensure => present,
+    path   => '/etc/fstab',
+    line   => '#<volume>  /var  <fstype>  nodev 0 0',
+    #match  => '\*/var',
+  }
+
+
+  mount { '(1.1.7) /var/tmp is bound to /tmp':
+    ensure  => 'mounted',
+    name    => '/var/tmp',
+    device  => '/tmp',
+    fstype  => 'none',
+    options => 'bind',
+  }
+}#EOF
