751209df2219beb974ef00c7b945cd914771547d
Adam Buxton <buxton.adam@gmail.com>
2017-08-11 11:29:15 +0100

    update new rule for autfs

--- manifests/redhat7/rule/v_2_1_1/rule_1_1_22.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_1_22.pp
@@ -3,8 +3,8 @@ class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_22 {
 
   exec{ '1.1.22 - Disable Automounting (Scored)':
     command => 'systemctl disable autofs',
-    path    => '/sbin'
-    onlyif  => 'test `[ $(systemctl is-enabled autofs) == "enabled" ]',
+    path    => '/sbin:/bin',
+    onlyif  => 'test [$(systemctl is-enabled autofs) == "enabled"]',
   }
 
 }#EOFs
