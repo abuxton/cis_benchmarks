4a848b4c601f81b5376fab04dfc19448230c8cb6
Adam Buxton <buxton.adam@gmail.com>
2017-08-14 00:02:33 +0100

    system management

---
+++ manifests/redhat7/rule/v_2_1_1/rule_4_3.pp
@@ -0,0 +1,8 @@
+# 4.3 Ensure logrotate is configured (Not Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_3 {
+  if defined(Package['rsyslog']) or defined(Package['syslog-ng']) {
+    notify{ '# (4.3) - Ensure logrotate is configured (Not Scored)':
+    }
+  }
+
+} #EOF
