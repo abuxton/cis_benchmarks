b487fe9059b86e51651e37a4e8d6d05bee0703f6
Adam Buxton <buxton.adam@gmail.com>
2017-08-13 16:35:26 +0100

    auditd

---
+++ manifests/redhat7/rule/v_2_1_1/rule_4_1_12.pp
@@ -0,0 +1,14 @@
+# 4.1.12 Ensure use of privileged commands is collected (Scored)
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_1_12 (
+  Array $cis_audit_partitions = lookup("cis_benchmarks::${::cis_benchmarks::cis_version}::cis_audit_partitions",Array,'first',$cis_benchmarks::params::cis_cis_audit_partitions)
+  ) inherits ::cis_benchmarks::params{
+
+  include cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_1
+  $cis_audit_partitions.each |$partition| {
+
+    auditd::rule { '(4.1.4) - Ensure events that modify date and time information are collected (Scored), 64bit':
+      content => "-a always,exit -F path='${partition}' -F perm=x -F auid>=1000 -F auid!=4294967295 -k privileged",
+    }
+  }
+
+} #EOF
---
+++ manifests/redhat7/rule/v_2_1_1/rule_4_1_13.pp
@@ -0,0 +1,36 @@
+# 4.1.13 Ensure successful file system mounts are collected (Scored)
+# 4.1.14 Ensure file deletion events by users are collected (Scored)
+# 4.1.15 Ensure changes to system administration scope (sudoers) is collected (Scored)
+# 4.1.16 Ensure system administrator actions (sudolog) are collected (Scored)
+
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_1_13 {
+  include cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_1
+  auditd::rule { '(4.1.13) - Ensure successful file system mounts are collected (Scored), 64bit':
+    content => '-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts',
+    order   => 40,
+  }
+  auditd::rule { '(4.1.13) - Ensure successful file system mounts are collected (Scored), 32 bit':
+    content => '-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts',
+    order   => 41,
+  }
+  auditd::rule { '(4.1.14) Ensure file deletion events by users are collected (Scored), 64bit':
+    content => '-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete',
+    order   => 42,
+  }
+  auditd::rule { '(4.1.13) - Ensure successful file system mounts are collected (Scored), 32 bit':
+    content => '-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete',
+    order   => 43,
+  }
+  auditd::rule{'(4.1.15) - Ensure changes to system administration scope (sudoers) is collected (Scored), sudoers':
+    content => '-w /etc/sudoers -p wa -k scope',
+    order   => 44,
+  }
+  auditd::rule{'(4.1.15) - Ensure changes to system administration scope (sudoers) is collected (Scored), sudoers.d':
+    content => '-w /etc/sudoers.d -p wa -k scope',
+    order   => 45,
+  }
+  audit::rule{ '(4.1.16) - Ensure system administrator actions (sudolog) are collected (Scored)':
+    content => '-w /var/log/sudo.log -p wa -k actions',
+    order   => 46,
+  }
+} #EOF
