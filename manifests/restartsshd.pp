5e9ed9d05cca37997e2e82c64d0814c17e68a171
Adam Buxton <buxton.adam@gmail.com>
2017-08-15 12:20:49 +0100

    removed unrequired class

--- manifests/restartsshd.pp
+++
@@ -1,7 +0,0 @@
-class cis_benchmarks::restartsshd {
-  exec { 'restart ssh daemon':
-    path        => ['/sbin','/bin'],
-    command     => 'systemctl restart sshd',
-    refreshonly => true,
-  }
-} #EOF
