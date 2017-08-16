b91bb73a856e0d646777589be60570cd6fc4e9fe
Adam Buxton <buxton.adam@gmail.com>
2017-08-16 09:02:47 +0100

    missing var

--- manifests/redhat7/rule/v_2_1_1/rule_1_8.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_8.pp
@@ -1,5 +1,6 @@
 # 1.8 Ensure updates, patches, and additional security software are installed (Not Scored)
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_8 {
+  $updates = $::cis_benchmarks['yum_updates']
   if $updates == 'yum updates are available'
   {
     notify { '(1.8) - Yum updates are available for this host': }
