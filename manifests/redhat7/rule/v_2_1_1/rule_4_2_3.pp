e7cf3d38f65df9127e1fd277964fe28d97fe89e2
Adam Buxton <buxton.adam@gmail.com>
2017-08-18 15:17:57 +0100

    update for log rotate

--- manifests/redhat7/rule/v_2_1_1/rule_4_2_3.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_4_2_3.pp
@@ -1,6 +1,6 @@
 # 4.2.3 Ensure rsyslog or syslog-ng is installed (Scored)
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_4_2_3{
-  if defined(Package['rsyslog'])and defined(Package['syslog-ng'])  {
+  if (defined(Package['rsyslog']) or defined(Package['syslog-ng'])) or (!defined(Package['rsyslog']) and !defined(Package['syslog-ng'])) {
     notify{ '(4.2.3) - Ensure rsyslog or syslog-ng is installed (Scored)':
     }
   }
