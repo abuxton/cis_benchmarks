2e89d3ca348b87cc08b7debfaa06634e5175deaf
Adam Buxton <buxton.adam@gmail.com>
2017-08-18 19:00:27 +0100

    update common.yaml and default var

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 class cis_benchmarks(
   String $cis_version     = lookup('cis_benchmarks::version', String,'first','v_2_1_1'),
   Boolean $benchmark      = lookup('cis_benchmarks::benchmark', Boolean, 'first', $cis_benchmarks::params::benchmark),
-  Hash $exec_controls     = lookup("cis_benchmarks::${cis_version}::exec_control", Hash, 'first', $cis_benchmarks::params::exec_control),
+  Hash $exec_controls     = lookup("cis_benchmarks::${cis_version}::exec_controls", Hash, 'first', $cis_benchmarks::params::exec_controls),
   Optional[String] $cis_scripts_dir = undef,
   Optional[Array] $cis_scripts  = undef,
   ) inherits ::cis_benchmarks::params {
