50adbc1c0ae96353b03882e32d6609974f023c75
Adam Buxton <buxton.adam@gmail.com>
2017-08-18 14:27:03 +0100

    change exec to sysctl

--- manifests/redhat7/rule/v_2_1_1/rule_1_5_1.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_5_1.pp
@@ -16,10 +16,11 @@ class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_5_1 {
     line   => '* hard core 0',
     notify => Exec['1.5.1 - Disable Automounting (Scored)'],
   }
-  exec{ '1.5.1 - Disable Automounting (Scored)':
-    command => 'sysctl -w fs.suid_dumpable=0',
-    path    => '/sbin:/bin',
-    onlyif  => 'test [$(sysctl fs.suid_dumpable) == "fs.suid_dumpable = 0"]',
-  }
+
+  sysctl { '(1.5.1) - Disable Automounting (Scored)':
+  name   => 'fs.suid_dumpable',
+  ensure => 'present',
+  value  => '0',
+}
 
 } #EOF
