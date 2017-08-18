b1074859eed328a1d8db5d9f27cab5045adc21e4
Adam Buxton <buxton.adam@gmail.com>
2017-08-18 14:18:57 +0100

    split 1.4.1 1.4.2

--- manifests/redhat7/rule/v_2_1_1/rule_1_3_2.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_3_2.pp
@@ -1,20 +1,13 @@
 # 1.3.1 Ensure AIDE is installed (Scored)
 # 1.3.2 Ensure filesystem integrity is regularly checked (Scored)
-class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_3_1 (
+class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_3_2 (
   Optional[String] $cis_aide_hour = lookup("cis_benchmarks::${::cis_benchmarks::cis_version}::cis_aide_hour",String,'first',$cis_benchmarks::params::cis_aide_hour),
     ) inherits ::cis_benchmarks::params {
-  package { '(1.3.1) - AIDE is installed':
-      ensure => present,
-      name   => 'aide',
-  }
-  exec { '(1.3.1) - Initialize AIDE':
-    path    => [ '/usr/bin', '/usr/sbin', '/bin' ],
-    command => "aide --init -B 'database_out=file:/var/lib/aide/aide.db.gz'",
-    creates => '/var/lib/aide/aide.db.gz',
-  }
+
   cron { '(1.3.2) - cron AIDE':
     command => '/usr/sbin/aide --check',
     user    => 'root',
     hour    => [$cis_aide_hour],
+    require => Package['(1.3.1) - AIDE is installed'],
   }
 } #EOF
