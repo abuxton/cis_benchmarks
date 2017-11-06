ebe10790aec7c6f1f71249739b096aba2e831122
Adam Buxton <buxton.adam@gmail.com>
2017-11-06 16:06:08 +0000

    syntax corrected

--- tasks/audit_mount_var_tmp_1_1_7.sh
+++ tasks/audit_mount_var_tmp_1_1_7.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-# Puppet Task Name: 
+# Puppet Task Name:
 #
 # This is where you put the shell code for your task.
 #
-# You can write Puppet tasks in any language you want and it's easy to 
+# You can write Puppet tasks in any language you want and it's easy to
 # adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
-# http://puppet.com/docs/bolt/latest/converting_scripts_to_tasks.html 
-# 
-# Puppet tasks make it easy for you to enable others to use your script. Tasks 
-# describe what it does, explains parameters and which are required or optional, 
-# as well as validates parameter type. For examples, if parameter "instances" 
-# must be an integer and the optional "datacenter" parameter must be one of 
-# portland, sydney, belfast or singapore then the .json file 
+# http://puppet.com/docs/bolt/latest/converting_scripts_to_tasks.html
+#
+# Puppet tasks make it easy for you to enable others to use your script. Tasks
+# describe what it does, explains parameters and which are required or optional,
+# as well as validates parameter type. For examples, if parameter "instances"
+# must be an integer and the optional "datacenter" parameter must be one of
+# portland, sydney, belfast or singapore then the .json file
 # would include:
 #   "parameters": {
 #     "instances": {
 #       "description": "Number of instances to create",
 #       "type": "Integer"
-#     }, 
+#     },
 #     "datacenter": {
 #       "description": "Datacenter where instances will be created",
 #       "type": "Enum[portland, sydney, belfast, singapore]"
@@ -26,3 +26,10 @@
 #   }
 # Learn more at: https://puppet.com/docs/bolt/latest/task_metadata.html
 #
+var_mount_var="$(mount | grep /var/tmp)"
+
+if [ "$var_mount_var" != '' ] && [ "$var_mount_var" ~= 'tmpfs on /var/tmp/ type ext4 (rw,nosuid,nodev,noexec,relatime)' ]; then
+  echo '1.1.7 passed'
+else
+  echo "1.1.7 failed: $var_mount_var"
+fi
