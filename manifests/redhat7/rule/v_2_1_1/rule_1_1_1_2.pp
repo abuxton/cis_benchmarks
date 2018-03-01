16b4f25c227d1c25c02a4df9ed7e48754bb37322
Jo Rhett <jo.rhett@nuance.com>
2018-02-28 19:58:42 -0800

    Minor fixes for copy pasta comment problems

--- manifests/redhat7/rule/v_2_1_1/rule_1_1_1_2.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_1_1_2.pp
@@ -1,4 +1,4 @@
-# 1.1.1.1 Ensure mounting of cramfs filesystems is disabled (Scored)
+# 1.1.1.2 Ensure mounting of freevxfs filesystems is disabled (Scored)
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_2{
 include ::cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_prereq
 $file = $::cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_prereq::file
