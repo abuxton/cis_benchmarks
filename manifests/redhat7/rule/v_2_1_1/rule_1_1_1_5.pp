943419b6aa4601bffd357191b74f19c4c2b48d56
Jo Rhett <jo.rhett@nuance.com>
2018-02-28 19:58:42 -0800

    Minor fixes for copy pasta comment problems

--- manifests/redhat7/rule/v_2_1_1/rule_1_1_1_5.pp
+++ manifests/redhat7/rule/v_2_1_1/rule_1_1_1_5.pp
@@ -1,4 +1,4 @@
-# 1.1.1.5 Ensure mounting of cramfs filesystems is disabled (Scored)
+# 1.1.1.5 Ensure mounting of hfsplus filesystems is disabled (Scored)
 class cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_5{
 include ::cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_prereq
 $file = $::cis_benchmarks::redhat7::rule::v_2_1_1::rule_1_1_1_prereq::file
