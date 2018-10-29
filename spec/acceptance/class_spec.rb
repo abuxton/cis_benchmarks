d9b01795a3464662fd1f2940b0737824f7d5cb13
lmayorga <lmayorga@lm3corp.com>
2018-10-29 14:53:31 -0400

    remove debugging

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -14,10 +14,10 @@ describe 'cis_benchmarks class' do
       # Run it twice and test for idempotency
       #1.The first execution does create the script directory and drops the files
       # which means that facter was not able to execute the bash scripts but still made some changes(packages/files/permissions)
-      apply_manifest(pp,{:debug=> true, :catch_failures => true}) #2
+      apply_manifest(pp,{ :catch_failures => true}) #2
       #2. The second execution executed the bash scripts and also some other puppet code(some rules that notify will be executed)
       #   if there are some rules disabled then some bash scripts will not be invoked
-      apply_manifest(pp,{:debug=>true, :catch_failures => true}) #2
+      apply_manifest(pp,{ :catch_failures => true}) #2
       #3. this should not expect any changes
       #apply_manifest(pp,{:catch_changes  => true}) #0
     end
